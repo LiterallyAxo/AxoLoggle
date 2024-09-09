643c705b9e94d3709a070c0d26328d567b91dde5
Axo 3 <147970407+LiterallyAxo@users.noreply.github.com>
2024-09-09 10:58:32 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ setup(
     description='Logging tools but cooler',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/your_username/my_module',
-    author='Your Name',
-    author_email='your.email@example.com',
-    license='MIT',  # Or whatever license you're using
-    packages=find_packages(),  # Automatically find your packages (submodules)
+    url='https://github.com/LiterallyAxo/AxoLoggle',
+    author='Axo',
+    author_email='axo@itsaxo.lol',
+    license='MIT',
+    packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',  # Minimum Python version requirement
-    install_requires=[],  # List dependencies here (e.g., ['numpy', 'requests'])
+    python_requires='>=3.10',
+    install_requires=['colorama'],
 )
